1c06e8b34ab49dbb6f908c916cbeff2a3dd8bec3
Ishan Chakraborty <91935558+IshanChakraborty96@users.noreply.github.com>
2022-12-21 18:28:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+st.title("Streamlit Example")
 
 with st.echo(code_location='below'):
     total_points = st.slider(os.environ['KEY1'], 1, 5000, 2000)
